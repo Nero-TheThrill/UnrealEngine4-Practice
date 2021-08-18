376b1fbf3a35caf0a0a75ddc0bda2d4993685c60
JinwooChoi Laptop <imjinwoo98@gmail.com>
2021-08-17 18:55:09 -0700

    upload rest of the file

--- Source/ArenaBattle/ABPlayerController.h
+++ Source/ArenaBattle/ABPlayerController.h
@@ -16,5 +16,8 @@ class ARENABATTLE_API AABPlayerController : public APlayerController
 public:
 	virtual void PostInitializeComponents() override;
 	virtual void OnPossess(APawn* aPawn) override;
+
+protected:
+	virtual void BeginPlay() override;
 	
 };
